ff61eefe90be42e221a14a2b022ab59d85da424c
Luiz Henrique <luizhbtluiz@gmail.com>
2021-03-03 00:13:26 -0300

    adicionado a classe de gerenciamento de elementos do solo, permitindo ser adicionado qualquer quantidade de elementos necessarios

---
+++ src/main/java/com/mycompany/app/model/InformacoesDoSolo.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.model;
+
+import java.util.Date;
+
+public class InformacoesDoSolo {
+  
+    private String Elemento;
+    private Double QuantidateAtual;
+    private Double QuantidadeIdeal;
+    
+}
