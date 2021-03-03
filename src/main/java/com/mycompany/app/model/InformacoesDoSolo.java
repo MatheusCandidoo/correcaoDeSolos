173cbada53128c65cc260da4ab2d2e543696ede7
Luiz Henrique <luizhbtluiz@gmail.com>
2021-03-03 00:02:11 -0300

    adicionado a classe de gerenciamento das informações sobre a analise do solo

---
+++ src/main/java/com/mycompany/app/model/InformacoesDoSolo.java
@@ -0,0 +1,19 @@
+package com.mycompany.app.model;
+
+import java.util.Date;
+
+public class InformacoesDoSolo {
+  
+ 
+    private Double numeroTalhao;
+    private Double areaTotal;
+    private Double areaTalhao;
+    private String soloArgiloso;
+    private Integer profundideAnalise;
+    private Double numeroAnalise;
+    private Date dataAnalise;
+    private String responsavelTecnico;
+    private Integer talhao;
+    private Integer texturaSolo;
+    
+}
