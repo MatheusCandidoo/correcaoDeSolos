73bdc583e58e32aecb27fae6ab88647fe2ee2e1b
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2021-03-11 19:35:48 -0300

    adicionado os metodos getter e setters  para manipular os dados inseridos das análises

--- src/main/java/com/mycompany/app/model/InformacoesDaAnalise.java
+++ src/main/java/com/mycompany/app/model/InformacoesDaAnalise.java
@@ -15,5 +15,84 @@ public class InformacoesDaAnalise {
     private String responsavelTecnico;
     private Integer talhao;
     private Integer texturaSolo;
-    
+
+    public Double getNumeroTalhao() {
+        return numeroTalhao;
+    }
+
+    public void setNumeroTalhao(Double numeroTalhao) {
+        this.numeroTalhao = numeroTalhao;
+    }
+
+    public Double getAreaTotal() {
+        return areaTotal;
+    }
+
+    public void setAreaTotal(Double areaTotal) {
+        this.areaTotal = areaTotal;
+    }
+
+    public Double getAreaTalhao() {
+        return areaTalhao;
+    }
+
+    public void setAreaTalhao(Double areaTalhao) {
+        this.areaTalhao = areaTalhao;
+    }
+
+    public String getSoloArgiloso() {
+        return soloArgiloso;
+    }
+
+    public void setSoloArgiloso(String soloArgiloso) {
+        this.soloArgiloso = soloArgiloso;
+    }
+
+    public Integer getProfundideAnalise() {
+        return profundideAnalise;
+    }
+
+    public void setProfundideAnalise(Integer profundideAnalise) {
+        this.profundideAnalise = profundideAnalise;
+    }
+
+    public Double getNumeroAnalise() {
+        return numeroAnalise;
+    }
+
+    public void setNumeroAnalise(Double numeroAnalise) {
+        this.numeroAnalise = numeroAnalise;
+    }
+
+    public Date getDataAnalise() {
+        return dataAnalise;
+    }
+
+    public void setDataAnalise(Date dataAnalise) {
+        this.dataAnalise = dataAnalise;
+    }
+
+    public String getResponsavelTecnico() {
+        return responsavelTecnico;
+    }
+
+    public void setResponsavelTecnico(String responsavelTecnico) {
+        this.responsavelTecnico = responsavelTecnico;
+    }
+
+    public Integer getTalhao() {
+        return talhao;
+    }
+
+    public void setTalhao(Integer talhao) {
+        this.talhao = talhao;
+    }
+
+    public Integer getTexturaSolo() {
+        return texturaSolo;
+    }
+
+    public void setTexturaSolo(Integer texturaSolo) {
+        this.texturaSolo = texturaSolo;
+    }
 }
--- src/main/java/com/mycompany/app/model/InformacoesDoSolo.java
+++ src/main/java/com/mycompany/app/model/InformacoesDoSolo.java
@@ -7,5 +7,28 @@ public class InformacoesDoSolo {
     private String Elemento;
     private Double QuantidateAtual;
     private Double QuantidadeIdeal;
-    
+
+    public String getElemento() {
+        return Elemento;
+    }
+
+    public void setElemento(String elemento) {
+        Elemento = elemento;
+    }
+
+    public Double getQuantidateAtual() {
+        return QuantidateAtual;
+    }
+
+    public void setQuantidateAtual(Double quantidateAtual) {
+        QuantidateAtual = quantidateAtual;
+    }
+
+    public Double getQuantidadeIdeal() {
+        return QuantidadeIdeal;
+    }
+
+    public void setQuantidadeIdeal(Double quantidadeIdeal) {
+        QuantidadeIdeal = quantidadeIdeal;
+    }
 }
