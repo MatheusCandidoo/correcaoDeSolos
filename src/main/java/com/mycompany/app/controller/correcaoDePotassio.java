18eb9abc0cb4647da9785ae8c36b11cf83d8ba5c
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2021-04-01 02:46:45 -0300

    adicionado metodos adicionais ao calculo de aplicaçao de potassio

---
+++ src/main/java/com/mycompany/app/controller/correcaoDePotassio.java
@@ -0,0 +1,47 @@
+package com.mycompany.app.controller;
+
+public class correcaoDePotassio {
+
+    private Double participacaoPotassioDesejada;
+    private Integer fontePotassioEscolhida;
+    private Integer texturaDoSolo;
+    private Double potassioNoSolo;
+    private Double calcioNoSolo;
+    private Double magnesioNoSolo;
+    private Double hALNoSolo;
+
+
+    public double informacoesPotassio(int fontePotassioEscolhida) {
+        switch (fontePotassioEscolhida) {
+            case 1:
+                return 58;
+            case 2:
+                return 52;
+            case 3:
+                return 22;
+            case 4:
+                return 44;
+            default:
+                return 0.0;
+        }
+    }
+
+    public Double participacaoPotassioAposCorrecao() {
+        if ((participacaoPotassioDesejada) > 0.001) {
+            return participacaoPotassioDesejada;
+        }
+        return 0.0;
+    }
+
+    public Double participacaoAtualDoPotassio() {
+        return potassioNoSolo / ((calcioNoSolo + magnesioNoSolo + potassioNoSolo) + hALNoSolo) * 100;
+    }
+
+    public Double participacaoIdealPotassioCTC (){
+        if(texturaDoSolo == 1 || texturaDoSolo == 2){
+            return 3.0;
+        }
+        return 0.0;
+    }
+
+}
