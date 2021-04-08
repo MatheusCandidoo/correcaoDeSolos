7dfebf2b2f80cfe90685ff4607fae81369cb2833
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2021-04-07 21:30:47 -0300

    criado construtor e arrumado as variaveis de atribuicao para funcoes

--- src/main/java/com/mycompany/app/controller/CorrecaoDeFosforo.java
+++ src/main/java/com/mycompany/app/controller/CorrecaoDeFosforo.java
@@ -8,37 +8,43 @@ public class CorrecaoDeFosforo {
     private Double teorNoSoloAtual;
     private FonteDeFosforo fonteDeFosforo;
     private Double eficienciaDeFosforo;
-    private Double calculoTrasformacaoIdeal = transformacaoIdeal(teorNoSoloAtingir, teorNoSoloAtual);
-    private Double calculoTeorPentoxidoDeFosforo = ((teorDePentoxidoDeFosforo(fonteDeFosforo) * 2) * 2.29) * 100;
 
-    public double teorDePentoxidoDeFosforo(FonteDeFosforo fonteDoTeor) {
+    public CorrecaoDeFosforo(Double teorNoSoloAtingir, Double teorNoSoloAtual, FonteDeFosforo fonteDeFosforo, Double eficienciaDeFosforo) {
+        this.teorNoSoloAtingir = teorNoSoloAtingir;
+        this.teorNoSoloAtual = teorNoSoloAtual;
+        this.fonteDeFosforo = fonteDeFosforo;
+        this.eficienciaDeFosforo = eficienciaDeFosforo;
+    }
+
+
+    public Double teorDePentoxidoDeFosforo(FonteDeFosforo fonteDoTeor) {
         switch (fonteDoTeor) {
             case Superfosfato_Simples:
-                return 18;
+                return 18.0;
             case Superfosfato_Triplo:
-                return 41;
+                return 41.0;
             case MAP:
-                return 48;
+                return 48.0;
             case DAP:
-                return 45;
+                return 45.0;
             case Yoorin:
-                return 18;
+                return 18.0;
             case Fosfato_Arad:
-                return 33;
+                return 33.0;
             case Fosfato_Gafsa:
-                return 29;
+                return 29.0;
             case Fosfato_Daoui:
-                return 32;
+                return 32.0;
             case Fosf_Patos_Minas:
-                return 24;
+                return 24.0;
             case Escoria_de_Thomas:
                 return 18.5;
             case Acido_Fosforico:
-                return 52;
+                return 52.0;
             case Multif_Magnesiano:
-                return 18;
+                return 18.0;
         }
-        return 0;
+        return 0.0;
     }
 
     public Double transformacaoIdeal(Double teorNoSoloAtingir, Double teorNoSoloAtual) {
@@ -48,8 +54,16 @@ public class CorrecaoDeFosforo {
         return (teorNoSoloAtingir - teorNoSoloAtual);
     }
 
+    public Double calculoTrasformacaoIdeal(){
+        return transformacaoIdeal(teorNoSoloAtingir, teorNoSoloAtual);
+    }
+
+    public Double calculoTeorPentoxidoDeFosforo(){
+        return ((teorDePentoxidoDeFosforo(fonteDeFosforo) * 2) * 2.29) * 100;
+    }
+
     public Double quantidadeAplicarFosforo() {
-        return (calculoTeorPentoxidoDeFosforo / eficienciaDeFosforo) * 100 / calculoTrasformacaoIdeal;
+        return (calculoTeorPentoxidoDeFosforo() / eficienciaDeFosforo) * 100 / calculoTrasformacaoIdeal();
     }
 
 }
