10f36b4c75582b61b03946692969f76ab5211175
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2021-04-25 23:09:17 -0300

    adicionado calculo dos nutrientes resultantes da correcao de fosforo

--- src/main/java/com/mycompany/app/controller/CorrecaoDeFosforo.java
+++ src/main/java/com/mycompany/app/controller/CorrecaoDeFosforo.java
@@ -94,6 +94,78 @@ public class CorrecaoDeFosforo {
         return 0.0;
     }
 
+    public double calculoNutrienteResultanteCorrecao1(FonteDeFosforo fonteDoTeor) {
+        switch (fonteDoTeor) {
+            case Superfosfato_Simples:
+                return  calculoDeEficienciaDaTranformacaoIdeal() * 0.10;
+            case Yoorin:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 18 * 0.15;
+            case Multif_Magnesiano:
+                return  calculoDeEficienciaDaTranformacaoIdeal() * 0.11;
+        }
+        return 0.0;
+    }
+
+    public String nomeNutrienteResultanteCorrecao1(FonteDeFosforo fonteDoTeor) {
+        switch (fonteDoTeor) {
+            case Superfosfato_Simples:
+            case Yoorin:
+                return  "enxofre";
+            case Multif_Magnesiano:
+                return "magnesio";
+        }
+        return "";
+    }
+
+    public double calculoNutrienteResultanteCorrecao2(FonteDeFosforo fonteDoTeor) {
+        switch (fonteDoTeor) {
+            case Superfosfato_Simples:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 18 * 0.28;
+            case Superfosfato_Triplo:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 41 * 0.20;
+            case MAP:
+                return calculoDeEficienciaDaTranformacaoIdeal() / 48 * 0.09;
+            case DAP:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 45 * 0.16;
+            case Yoorin:
+                return calculoDeEficienciaDaTranformacaoIdeal() / 18 * 0.28;
+            case Fosfato_Arad:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 33 * 0.52;
+            case Fosfato_Gafsa:
+                return calculoDeEficienciaDaTranformacaoIdeal() / 29 * 0.52;
+            case Fosfato_Daoui:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 32 * 0.45;
+            case Fosf_Patos_Minas:
+                return calculoDeEficienciaDaTranformacaoIdeal() / 24 * 0.28;
+            case Escoria_de_Thomas:
+                return calculoDeEficienciaDaTranformacaoIdeal() / 18.5 * 0.44;
+            case Acido_Fosforico:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 52 * 0.00;
+            case Multif_Magnesiano:
+                return  calculoDeEficienciaDaTranformacaoIdeal() / 18 * 0.18;
+        return 0.0;
+    }}
+
+    public String nomeNutrienteResultanteCorrecao2(FonteDeFosforo fonteDoTeor) {
+        switch (fonteDoTeor) {
+            case Superfosfato_Simples:
+            case Superfosfato_Triplo:
+            case Yoorin:
+            case Fosfato_Arad:
+            case Fosfato_Gafsa:
+            case Fosfato_Daoui:
+            case Fosf_Patos_Minas:
+            case Escoria_de_Thomas:
+            case Multif_Magnesiano:
+                return "calcio";
+            case MAP:
+            case DAP:
+                return "nitrogenio";
+            case Acido_Fosforico:
+                return "";
+        }
+        return "";
+    }
 }
 
 
